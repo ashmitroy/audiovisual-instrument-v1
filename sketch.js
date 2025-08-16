821c703c22f6ea7601ee3060603c616b1cee78b6
Ashmit Deb Roy <72800892+ashmitroy@users.noreply.github.com>
2025-08-16 11:41:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,8 +29,7 @@ function draw(){
     let freq = map(mouseX, 0, width, 100, 1000);
 
     // map mouseY to volume (0–0.5)
-    let vol = map(mouseY, height, 0, 0, 0.5);
-
+    let vol = map(mouseY, 0, height, 1, 0);
     osc.freq(freq);
     osc.amp(vol);
 
